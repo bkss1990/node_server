4c9df50768166203154155561a2b03c804fb657b
Bablu Kumar <bablu@rudrainnovatives.com>
2018-08-20 19:17:48 +0530

    removed extra comments

--- controller/auth/AuthController.js
+++ controller/auth/AuthController.js
@@ -1,6 +1,4 @@
-var express = require('express');
 var User = require('../../models/User')
-
 /**
  * Configure JWT
  */
@@ -12,55 +10,23 @@ exports.login = (req, res)=>{
   User.findOne({ email: req.body.email }, function (err, user) {
     if (err) return res.status(500).send('Error on the server.');
     if (!user) return res.status(404).send('No user found.');
-    
-    // check if the password is valid
     var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
     if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
-
-    // if user is found and password is valid
     // create a token
     var token = jwt.sign({ id: user._id }, config.secret, {
       expiresIn: 86400 // expires in 24 hours
     });
-
     // return the information including token as JSON
     res.status(200).send({ auth: true, token: token });
   });
-
 }
-
-/* router.post('/login', function(req, res) {
-
-  User.findOne({ email: req.body.email }, function (err, user) {
-    if (err) return res.status(500).send('Error on the server.');
-    if (!user) return res.status(404).send('No user found.');
-    
-    // check if the password is valid
-    var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
-    if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
-
-    // if user is found and password is valid
-    // create a token
-    var token = jwt.sign({ id: user._id }, config.secret, {
-      expiresIn: 86400 // expires in 24 hours
-    });
-
-    // return the information including token as JSON
-    res.status(200).send({ auth: true, token: token });
-  });
-
-});
- */
 
  exports.logout = (req, res)=>{
 
   res.status(200).send({ auth: false, token: null });
 
  }
-/* router.get('/logout', function(req, res) {
-  res.status(200).send({ auth: false, token: null });
-});
- */
+
 
 exports.register = (req, res)=>{
   var hashedPassword = bcrypt.hashSync(req.body.password, 8);
@@ -81,30 +47,6 @@ exports.register = (req, res)=>{
     res.status(200).send({ auth: true, token: token });
   });
 }
-/* router.post('/register', function(req, res) {
-
-  var hashedPassword = bcrypt.hashSync(req.body.password, 8);
-
-  User.create({
-    name : req.body.name,
-    email : req.body.email,
-    password : hashedPassword
-  }, 
-  function (err, user) {
-    if (err) return res.status(500).send("There was a problem registering the user`.");
-
-    // if user is registered without errors
-    // create a token
-    var token = jwt.sign({ id: user._id }, config.secret, {
-      expiresIn: 86400 // expires in 24 hours
-    });
-
-    res.status(200).send({ auth: true, token: token });
-  });
-
-});
- */
-
 exports.me = (req, res)=>{
   User.findById(req.userId, { password: 0 }, function (err, user) {
     if (err) return res.status(500).send("There was a problem finding the user.");
@@ -114,8 +56,3 @@ exports.me = (req, res)=>{
 
 
 }
-/* router.get('/me', VerifyToken, function(req, res, next) {
-
-});
- */
-// module.exports = router;
