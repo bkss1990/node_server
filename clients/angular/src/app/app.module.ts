7262966eba892158009c435083dae69151d79965
Bablu Kumar <bablu@rudrainnovatives.com>
2018-08-21 19:13:09 +0530

    worked on social login with angular

--- clients/angular/src/app/app.module.ts
+++ clients/angular/src/app/app.module.ts
@@ -1,17 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule }    from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import {
+  SocialLoginModule,
+  AuthServiceConfig,
+  GoogleLoginProvider,
+  FacebookLoginProvider,
+  LinkedinLoginProvider,
+  AuthService
+} from "angular-6-social-login";
+
 import { AppComponent } from './app.component';
 import { UiModule } from './ui/ui.module';
 import { HomeComponent } from './home/home.component';
-import { routing }   from './app.routing';
+import { routing } from './app.routing';
 import { AlertComponent } from './_directives';
 import { AuthGuard } from './_guards';
 import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 import { AlertService, AuthenticationService, UserService } from './_services';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+// Configs 
+export function getAuthServiceConfigs() {
+  let config = new AuthServiceConfig(
+    [
+      {
+        id: FacebookLoginProvider.PROVIDER_ID,
+        provider: new FacebookLoginProvider("1874353639540976")
+      },
+      {
+        id: GoogleLoginProvider.PROVIDER_ID,
+        provider: new GoogleLoginProvider("345746003850-v7omnnb7e7n1u7h8tt8aa4hmf5kmb57u.apps.googleusercontent.com")
+      },
+    /*   {
+        id: LinkedinLoginProvider.PROVIDER_ID,
+        provider: new LinkedinLoginProvider("1098828800522-m2ig6bieilc3tpqvmlcpdvrpvn86q4ks.apps.googleusercontent.com")
+      }, */
+    ]
+  );
+  return config;
+}
 
 @NgModule({
   declarations: [
@@ -34,10 +64,14 @@ import { RegisterComponent } from './register/register.component';
     AlertService,
     AuthenticationService,
     UserService,
+    AuthService,
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-
-],
+    {
+      provide: AuthServiceConfig,
+      useFactory: getAuthServiceConfigs
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- clients/angular/src/app/login/login.component.ts
+++ clients/angular/src/app/login/login.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+    AuthService,
+    FacebookLoginProvider,
+    GoogleLoginProvider,
+    LinkedinLoginProvider
+} from 'angular-6-social-login';
 import { first } from 'rxjs/operators';
 
 import { AlertService, AuthenticationService } from '../_services';
@@ -22,6 +28,7 @@ export class LoginComponent implements OnInit {
       private route: ActivatedRoute,
       private router: Router,
       private authenticationService: AuthenticationService,
+      private socialAuthService: AuthService,
       private alertService: AlertService) {}
 
   ngOnInit() {
@@ -39,6 +46,30 @@ export class LoginComponent implements OnInit {
 
   // convenience getter for easy access to form fields
   get f() { return this.loginForm.controls; }
+
+
+  public socialSignIn(socialPlatform : string) {
+    let socialPlatformProvider;
+    if(socialPlatform == "facebook"){
+      socialPlatformProvider = FacebookLoginProvider.PROVIDER_ID;
+    }else if(socialPlatform == "google"){
+      socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
+    } else if (socialPlatform == "linkedin") {
+      socialPlatformProvider = LinkedinLoginProvider.PROVIDER_ID;
+    }
+    
+    this.socialAuthService.signIn(socialPlatformProvider).then(
+      (userData) => {
+        console.log(socialPlatform+" sign in data : " , userData);
+        // Now sign-in with userData
+        // ...
+        localStorage.setItem('currentUser', JSON.stringify(userData));
+        this.router.navigate([this.returnUrl]);
+
+ 
+      }
+    );
+  }
 
   onSubmit() {
       this.submitted = true;
